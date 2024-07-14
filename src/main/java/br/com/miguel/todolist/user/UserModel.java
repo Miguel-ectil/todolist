0ef0bfe72db6df7112f08a0546d1b93696c7fb5a
Miguel-ectil <ectilmiguelmiguelectil.com>
2024-07-14 15:43:45 -0300

    [ADD] simplificando a utilização do getters e setters com a biblioteca-lib 'Lombok'

--- src/main/java/br/com/miguel/todolist/user/UserModel.java
+++ src/main/java/br/com/miguel/todolist/user/UserModel.java
@@ -1,35 +1,12 @@
 package br.com.miguel.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
+
     private String username;
     private String name;
     private String password;
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setname(String name) {
-        this.name = name;
-    }
-
-    public String getname() {
-        return name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-    /**
-     * Metodos de acesso aos dados privados
-     * getters and setters
-     */
 }
