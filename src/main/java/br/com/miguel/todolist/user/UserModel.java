7096efb25659b324a005328c5e0dff8de81ece6c
Miguel-ectil <ectilmiguelmiguelectil.com>
2024-07-14 00:25:29 -0300

    [ADD and UPD] Acessando dados de uma requisição private

--- src/main/java/br/com/miguel/todolist/user/UserModel.java
+++ src/main/java/br/com/miguel/todolist/user/UserModel.java
@@ -1,7 +1,35 @@
 package br.com.miguel.todolist.user;
 
 public class UserModel {
-    public String username;
-    public String name;
-    public String password;
+    private String username;
+    private String name;
+    private String password;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setname(String name) {
+        this.name = name;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    /**
+     * Metodos de acesso aos dados privados
+     * getters and setters
+     */
 }
