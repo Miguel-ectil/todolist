b5b1ed9696a9a82c3ddd383faf60e8ae79da0411
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2024-10-07 14:59:30 -0300

    [ADD] Finalizando a criptografia da senha

--- src/main/java/br/com/miguel/todolist/user/UserController.java
+++ src/main/java/br/com/miguel/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 /**
  * Modificador
  * public
@@ -17,33 +19,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    /**
-     * string (texto)
-     * Integer (int) Números inteiros
-     * Double (double) Números 0.0000
-     * Float (float) Números 0.000
-     * char (A C)
-     * Date (data)
-     * void
-     */
-    /*
-     * Body
-     */
-    @Autowired
-    private IUserRepository userRepository;
+  /**
+   * string (texto)
+   * Integer (int) Números inteiros
+   * Double (double) Números 0.0000
+   * Float (float) Números 0.000
+   * char (A C)
+   * Date (data)
+   * void
+   */
+  /*
+   * Body
+   */
+  @Autowired
+  private IUserRepository userRepository;
 
-    @PostMapping("/")
-    public ResponseEntity create(@RequestBody UserModel userModel) {
-      var user = this.userRepository.findByUsername(userModel.getUsername());
-      
-      // Verifica se o usuário já existe
-      if (user != null) {
-        // Message errror
-        // Status Code
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
-      }
+  @PostMapping("/")
+  public ResponseEntity create(@RequestBody UserModel userModel) {
+    var user = this.userRepository.findByUsername(userModel.getUsername());
 
-      var userCreated = this.userRepository.save(userModel);
-      return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    // Verifica se o usuário já existe
+    if (user != null) {
+      // Message errror
+      // Status Code
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
     }
+
+    var passwordHashred = BCrypt.withDefaults()
+        .hashToString(12, userModel.getPassword().toCharArray());
+
+    userModel.setPassword(passwordHashred);
+
+    var userCreated = this.userRepository.save(userModel);
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+  }
 }
