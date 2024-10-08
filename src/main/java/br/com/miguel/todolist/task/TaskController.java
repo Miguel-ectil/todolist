be7cb15eed5f1eb097983ff90456cfecea652b77
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2024-10-08 16:20:00 -0300

    [ADD] Adicionando dois arquivos ItaskRepository e TaskController, comecando a criar as apis que criaram as tarefas para a tabela

---
+++ src/main/java/br/com/miguel/todolist/task/TaskController.java
@@ -0,0 +1,10 @@
+package br.com.miguel.todolist.task;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    
+}
