e3316d237bf64f67b56785c8f7d0330657303633
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2024-10-10 09:40:08 -0300

    [ADD and UPD] Adicionando novo arquivo de filtro ela sera responsavel por verificar se o usuário está cadastrado ou não para que assim possa cadastrar as terefa

--- src/main/java/br/com/miguel/todolist/task/TaskController.java
+++ src/main/java/br/com/miguel/todolist/task/TaskController.java
@@ -15,6 +15,7 @@ public class TaskController {
 
   @PostMapping("/")
   public TaskModel create(@RequestBody TaskModel taskModel) {
+    System.out.println("Chegou no Controller");
     var task = this.taskRepository.save(taskModel);
     return task;
   }
--- src/main/java/br/com/miguel/todolist/task/TaskModel.java
+++ src/main/java/br/com/miguel/todolist/task/TaskModel.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
