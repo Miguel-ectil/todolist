d446e8aa9880e1c664214f25d5cac793039a2eb6
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2024-10-11 10:11:02 -0300

    [ADD] Resgatando a senha criptografado cadastrada ao criar a task

--- src/main/java/br/com/miguel/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/miguel/todolist/filter/FilterTaskAuth.java
@@ -3,23 +3,32 @@ package br.com.miguel.todolist.filter;
 import java.io.IOException;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class FilterTaskAuth implements Filter {
+public class FilterTaskAuth extends OncePerRequestFilter {
 
   @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-    throws IOException, ServletException {
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    throws ServletException, IOException {
 
-    // Executar alguma ação        
-    System.out.println("Chegou no filtro");
-    chain.doFilter(request, response);
-}
+    // Pegar a autenticação (usuário e senha)
+    var authorization = request.getHeader("Authorization");
+    
+    var user_password = authorization.substring("Basic".length()).trim();
+    System.out.println("Authorization");
+    System.out.println(user_password);
+
+    // Validar usuário
+    // Validar senha
+
+    filterChain.doFilter(request, response);
+  }
+
     
 }
