eb3142f8cca9767ee968b4abb8348dc888c82f0a
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2024-10-14 16:04:17 -0300

    [ADD and UPD] Adicionando validação para trazer apenas as tarefas do cliente autenticado

--- src/main/java/br/com/miguel/todolist/task/TaskController.java
+++ src/main/java/br/com/miguel/todolist/task/TaskController.java
@@ -1,11 +1,13 @@
 package br.com.miguel.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,7 +34,7 @@ public class TaskController {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST)
           .body("A data de inicio / data de término deve ser maior do que a data atual");
     }
-    
+
     if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST)
           .body("A data de inicio deve ser menor do que a data de término");
@@ -40,5 +42,12 @@ public class TaskController {
 
     var task = this.taskRepository.save(taskModel);
     return ResponseEntity.status(HttpStatus.OK).body(task);
+  }
+  
+  @GetMapping("/")
+  public List<TaskModel> list(HttpServletRequest request) {
+    var idUser = request.getAttribute("idUser");
+    var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+    return tasks;
   }
 }
