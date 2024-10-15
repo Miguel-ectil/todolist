9f5bdb05d848ab4629f6d7ca4a29158f490cddad
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2024-10-15 11:50:11 -0300

    [ADD] Adicionando requisição de atualizar tasks requisição 'PUT'

--- src/main/java/br/com/miguel/todolist/task/TaskController.java
+++ src/main/java/br/com/miguel/todolist/task/TaskController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,5 +51,13 @@ public class TaskController {
     var idUser = request.getAttribute("idUser");
     var tasks = this.taskRepository.findByIdUser((UUID) idUser);
     return tasks;
+  }
+
+  // http>//localhost:8080/tasks/480b5442-aec9-477a-85fd-05f835e77faa
+  @PutMapping("/{id}")
+  public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+    
+    taskModel.setId(id);
+    return this.taskRepository.save(taskModel);
   }
 }
